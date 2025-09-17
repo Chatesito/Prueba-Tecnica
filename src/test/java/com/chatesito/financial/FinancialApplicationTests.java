237112ede6daea6e0094bccd262b723d6dad1a8a
Chatesito <juandavidriveracrack@gmail.com>
2025-09-17 12:48:58 -0500

    Project setup in Spring Boot

---
+++ src/test/java/com/chatesito/financial/FinancialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatesito.financial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
